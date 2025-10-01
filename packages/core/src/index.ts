4e2ed5293088b50a7a643113822e1bf34adba56c
Fazlul Hoque <fazlul@myalice.ai>
2025-10-01 15:48:34 +0600

    start-over

--- packages/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Core exports
-export * from './core/ai/generator.js';
-export * from './core/ai/providers.js';
-export * from './core/config/manager.js';
-export * from './core/config/schema.js';
-export * from './core/git/analyzer.js';
-export * from './utils/Logger.js';
